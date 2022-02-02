b66d8029c7b670b01b7a193789e80de9c5c2c700
unknown <toukamornela5@gmail.com>
2022-02-02 20:26:16 +0100

    getter et setter

---
+++ robot.java
@@ -0,0 +1,17 @@
+
+//utilisation des getters et des setters
+public String getNom() {
+        return nom;
+        }
+
+public int getVie() {
+        return vie;
+        }
+
+public void setVie(int vie) {
+        this.vie = vie;
+        }
+
+public void setNom(String nom) {
+        this.nom = nom;
+        }
